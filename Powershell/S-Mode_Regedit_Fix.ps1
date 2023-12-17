08ef23650face9a47096c0e6940be4c81c092f47
unknown <panamakunal@gmail.com>
2023-12-17 14:45:18 -0500

    Added Error handling to S-Mode Regedit Fix. Now prints reg value after completion of command.

--- Powershell/S-Mode_Regedit_Fix.ps1
+++ Powershell/S-Mode_Regedit_Fix.ps1
@@ -1,1 +1,8 @@
-Set-ItemProperty -Path "HKLM:\SYSTEM\ControlSet001\Control\CI\Policy" -Name "SkuPolicyRequired" - Value 0
+try {
+	Set-ItemProperty -Path "HKLM:\SYSTEM\ControlSet001\Control\CI\Policy" -Name "SkuPolicyRequired" -Value 0 -ErrorAction Stop
+	$value = Get-ItemProperty -Path "HKLM:\SYSTEM\ControlSet001\Control\CI\Policy" -Name "SkuPolicyRequired" 
+	Write-Host "Registry value for SkuPolicyRequired is now set to:" $value.SkuPolicyRequired
+}
+catch {
+ Write-Host "Error occurred: $_"
+}
